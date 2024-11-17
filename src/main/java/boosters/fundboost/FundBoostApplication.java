56c3582f8192419438423b4fea7957f00164482f
88dldl <chltnrgml0999@gmail.com>
2024-11-18 00:14:08 +0900

    Initial commit

---
+++ src/main/java/boosters/fundboost/FundBoostApplication.java
@@ -0,0 +1,13 @@
+package boosters.fundboost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundBoostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundBoostApplication.class, args);
+    }
+
+}
---
+++ src/test/java/boosters/fundboost/FundBoostApplicationTests.java
@@ -0,0 +1,13 @@
+package boosters.fundboost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundBoostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
