031c345a104ee752e5e47de563d8b540b7b9a26c
88dldl <chltnrgml0999@gmail.com>
2024-11-18 20:34:19 +0900

    refactor: 다양한 오류 상태,이유를 처리할 수 있도록 리팩토링

--- src/main/java/boosters/fundboost/global/response/BaseResponse.java
+++ src/main/java/boosters/fundboost/global/response/BaseResponse.java
@@ -1,6 +1,8 @@
 package boosters.fundboost.global.response;
 
 import boosters.fundboost.global.response.code.BaseCode;
+import boosters.fundboost.global.response.code.ErrorReasonDTO;
+import boosters.fundboost.global.response.code.status.ErrorStatus;
 import boosters.fundboost.global.response.code.status.SuccessStatus;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -22,9 +24,8 @@ public class BaseResponse<T> {
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private T result;
 
-    public static <T> BaseResponse<T> onSuccess(T data) {
-        return new BaseResponse<>(
-                true, SuccessStatus._OK.getCode(), SuccessStatus._OK.getMessage(), data);
+    public static <T> BaseResponse<T> onSuccess(T data, SuccessStatus status) {
+        return new BaseResponse<>(true, status.getCode(), status.getMessage(), data);
     }
 
     public static <T> BaseResponse<T> of(BaseCode code, T result) {
@@ -35,7 +36,15 @@ public class BaseResponse<T> {
                 result);
     }
 
-    public static <T> BaseResponse<T> onFailure(String code, String message, T data) {
-        return new BaseResponse<>(false, code, message, data);
+    public static <T> BaseResponse<T> onFailure(ErrorStatus errorCode, T data) {
+        return new BaseResponse<>(false, errorCode.getCode(), errorCode.getMessage(), data);
+    }
+
+    public static <T> BaseResponse<T> onFailure(ErrorStatus errorCode) {
+        return onFailure(errorCode, null);
+    }
+
+    public static <T> BaseResponse<T> onFailure(ErrorReasonDTO reason, T data) {
+        return new BaseResponse<>(false, reason.getCode(), reason.getMessage(), data);
     }
 }
