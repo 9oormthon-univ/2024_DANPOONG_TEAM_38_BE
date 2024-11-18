104d6820aaf6b589b4db1abee17e60f98a913d9c
88dldl <chltnrgml0999@gmail.com>
2024-11-19 01:15:56 +0900

    feat: 리프레시 토큰 재발급 API 구현

--- src/main/java/boosters/fundboost/user/auth/controller/AuthController.java
+++ src/main/java/boosters/fundboost/user/auth/controller/AuthController.java
@@ -3,7 +3,9 @@ package boosters.fundboost.user.auth.controller;
 import boosters.fundboost.global.response.BaseResponse;
 import boosters.fundboost.global.response.code.status.SuccessStatus;
 import boosters.fundboost.global.security.handler.annotation.AuthUser;
+import boosters.fundboost.global.security.handler.annotation.AuthToken;
 import boosters.fundboost.user.auth.dto.AuthResponse.OAuthResponse;
+import boosters.fundboost.user.auth.dto.AuthResponse.TokenRefreshResponse;
 import boosters.fundboost.user.auth.service.AuthService;
 import boosters.fundboost.user.domain.User;
 import io.swagger.v3.oas.annotations.Operation;
@@ -14,6 +16,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,5 +48,16 @@ public class AuthController {
     public BaseResponse<Long> kakaoLogout(@Parameter(name = "user", hidden = true) @AuthUser User user) {
         authService.kakaoLogout(user.getId());
         return BaseResponse.onSuccess(SuccessStatus._OK, user.getId());
+    }
+
+    @Operation(summary = "토큰 재발급 API", description = "리프레시 토큰을 검증하고 토큰을 재발급 합니다._숙희")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
+            @ApiResponse(responseCode = "TOKEN400", description = "NOT_FOUND_TOKEN, 토큰을 찾을 수 없습니다."),
+            @ApiResponse(responseCode = "TOKEN402", description = "INVALID_TOKEN, 유효하지 않은 토큰입니다."),
+    })
+    @PostMapping("/kakao/refresh")
+    public BaseResponse<TokenRefreshResponse> refresh(@AuthToken String refreshToken) {
+        return BaseResponse.onSuccess(SuccessStatus._OK, authService.refresh(refreshToken));
     }
 }
