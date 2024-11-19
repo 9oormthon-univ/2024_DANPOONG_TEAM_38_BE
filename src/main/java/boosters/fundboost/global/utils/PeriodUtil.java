6ea6fc260e26f740421f939355a1a95310955b8b
88dldl <chltnrgml0999@gmail.com>
2024-11-19 19:51:18 +0900

    refactor: 가독성을 위해 문자열 연결 대신 String.format 사용

--- src/main/java/boosters/fundboost/global/utils/PeriodUtil.java
+++ src/main/java/boosters/fundboost/global/utils/PeriodUtil.java
@@ -5,8 +5,9 @@ import java.time.format.DateTimeFormatter;
 
 public class PeriodUtil {
     public static String localDateToPeriodFormat(LocalDate startDate, LocalDate endDate) {
-        return LocalDateToString(startDate) + "-" + LocalDateToString(endDate);
+        return String.format("%s-%s", LocalDateToString(startDate), LocalDateToString(endDate));
     }
+
     private static String LocalDateToString(LocalDate date) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
         return date.format(formatter);
