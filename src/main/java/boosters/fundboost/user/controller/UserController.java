3807b8c36be238c7860a6f173ca96ec087f6116f
88dldl <chltnrgml0999@gmail.com>
2024-11-20 03:35:03 +0900

    feat: 관심있는 프로젝트 조회 API 구현

--- src/main/java/boosters/fundboost/user/controller/UserController.java
+++ src/main/java/boosters/fundboost/user/controller/UserController.java
@@ -3,6 +3,7 @@ package boosters.fundboost.user.controller;
 import boosters.fundboost.global.response.BaseResponse;
 import boosters.fundboost.global.response.code.status.SuccessStatus;
 import boosters.fundboost.global.security.handler.annotation.AuthUser;
+import boosters.fundboost.project.dto.response.ProjectPreviewResponse;
 import boosters.fundboost.user.domain.User;
 import boosters.fundboost.user.dto.response.UserMyPageResponse;
 import boosters.fundboost.user.service.UserService;
@@ -12,8 +13,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -30,5 +33,15 @@ public class UserController {
     @GetMapping("/mypage")
     public BaseResponse<UserMyPageResponse> myPage(@Parameter(name = "user", hidden = true) @AuthUser User user) {
         return BaseResponse.onSuccess(SuccessStatus._OK, userService.getMyPage(user));
+    }
+
+    @Operation(summary = "관심 프로젝트 조회 API", description = "관심 프로젝트를 조회 합니다._숙희")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
+    })
+    @GetMapping("/favorites")
+    public BaseResponse<Page<ProjectPreviewResponse>> getFavProjects(@RequestParam(value = "page", defaultValue = "0", required = false) int page,
+                                                                     @Parameter(name = "user", hidden = true) @AuthUser User user) {
+        return BaseResponse.onSuccess(SuccessStatus._OK, userService.getFavProjects(user.getId(), page));
     }
 }
