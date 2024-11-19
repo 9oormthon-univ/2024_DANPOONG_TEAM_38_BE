33b47d71237ae61c9d4b402deed9ee76d7abaf49
88dldl <chltnrgml0999@gmail.com>
2024-11-20 02:00:51 +0900

    feat: 유저의 마이페이지 응답 DTO, 컨버터 생성

---
+++ src/main/java/boosters/fundboost/user/converter/UserMyPageConverter.java
@@ -0,0 +1,16 @@
+package boosters.fundboost.user.converter;
+
+import boosters.fundboost.user.domain.User;
+import boosters.fundboost.user.dto.response.UserMyPageResponse;
+
+public class UserMyPageConverter {
+    public static UserMyPageResponse toUserMyPageResponse(User user, long followingCount, long followerCount) {
+        return UserMyPageResponse.builder()
+                .image(user.getImage())
+                .link(user.getLink())
+                .tag(user.getTag().getValue())
+                .followingCount(followingCount)
+                .followerCount(followerCount)
+                .build();
+    }
+}
