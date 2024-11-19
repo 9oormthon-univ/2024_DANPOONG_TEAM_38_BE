a4bd3cfa939bbd69f28a36e854d19cb0d4651676
88dldl <chltnrgml0999@gmail.com>
2024-11-19 16:24:32 +0900

    refactor: 도시명을 적절한 영어로 수정

--- src/main/java/boosters/fundboost/project/domain/enums/Region.java
+++ src/main/java/boosters/fundboost/project/domain/enums/Region.java
@@ -2,14 +2,14 @@ package boosters.fundboost.project.domain.enums;
 
 public enum Region {
     SEOUL("서울특별시"),
-    INCHON("인천광역시"),
+    INCHEON("인천광역시"),
     DAEGU("대구광역시"),
     BUSAN("부산광역시"),
     GWANGJU("광주광역시"),
     DAEJEON("대전광역시"),
     ULSAN("울산광역시"),
     JEJU("제주특별자치도"),
-    CHUNGCHEON("충청도"),
+    CHUNGCHEONG("충청도"),
     GYEONGGI("경기도"),
     GANGWON("강원도"),
     JEOLLA("전라도"),
