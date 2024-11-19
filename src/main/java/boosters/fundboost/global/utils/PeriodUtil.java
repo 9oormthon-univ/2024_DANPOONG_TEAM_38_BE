fe55484a3c034f71017813ae7bfd105e57e3be71
88dldl <chltnrgml0999@gmail.com>
2024-11-19 15:57:28 +0900

    feat: 시작일, 종료일을 String으로 포맷팅하는 유틸리티 클래스 작성

---
+++ src/main/java/boosters/fundboost/global/utils/PeriodUtil.java
@@ -0,0 +1,14 @@
+package boosters.fundboost.global.utils;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class PeriodUtil {
+    public static String localDateToPeriodFormat(LocalDate startDate, LocalDate endDate) {
+        return LocalDateToString(startDate) + "-" + LocalDateToString(endDate);
+    }
+    private static String LocalDateToString(LocalDate date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+        return date.format(formatter);
+    }
+}
