54eb725cd62a94c790934f791a4f528b9ca049da
88dldl <chltnrgml0999@gmail.com>
2024-11-19 16:00:04 +0900

    feat: 진행률을 계산하는 계산 유틸리티 클래스 작성

---
+++ src/main/java/boosters/fundboost/global/utils/CalculatorUtil.java
@@ -0,0 +1,12 @@
+package boosters.fundboost.global.utils;
+
+import java.text.DecimalFormat;
+
+public class CalculatorUtil {
+    private static final String DECIMAL_PATTERN = "#.0";
+
+    public static double calculateProgressRate(long achievedAmount, long targetAmount) {
+        DecimalFormat df = new DecimalFormat(DECIMAL_PATTERN);
+        return Double.parseDouble(df.format(((double) achievedAmount / targetAmount) * 100));
+    }
+}
