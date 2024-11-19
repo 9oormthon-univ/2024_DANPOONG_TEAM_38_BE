12bc8603be97edc037175aed66ed4bdcde26b4a0
88dldl <chltnrgml0999@gmail.com>
2024-11-19 16:04:57 +0900

    refactor: 의미있는 숫자 상수화

--- src/main/java/boosters/fundboost/global/utils/CalculatorUtil.java
+++ src/main/java/boosters/fundboost/global/utils/CalculatorUtil.java
@@ -4,9 +4,10 @@ import java.text.DecimalFormat;
 
 public class CalculatorUtil {
     private static final String DECIMAL_PATTERN = "#.0";
+    private static final int PERCENTAGE = 100;
 
     public static double calculateProgressRate(long achievedAmount, long targetAmount) {
         DecimalFormat df = new DecimalFormat(DECIMAL_PATTERN);
-        return Double.parseDouble(df.format(((double) achievedAmount / targetAmount) * 100));
+        return Double.parseDouble(df.format(((double) achievedAmount / targetAmount) * PERCENTAGE));
     }
 }
