820f4b5ecee331b6056d50afd24433d73c55d1e5
88dldl <chltnrgml0999@gmail.com>
2024-11-21 05:47:54 +0900

    feat: 제안서 관련 예외 클래스 및 코드 추가

--- src/main/java/boosters/fundboost/global/response/code/status/ErrorStatus.java
+++ src/main/java/boosters/fundboost/global/response/code/status/ErrorStatus.java
@@ -41,6 +41,8 @@ public enum ErrorStatus implements BaseErrorCode {
     INVALID_EXTENSION(HttpStatus.BAD_REQUEST, "S3400", "지원하지 않는 확장자입니다."),
     // COMPANY
     COMPANY_NOT_FOUND(HttpStatus.NOT_FOUND, "COMPANY400", "기업을 찾을 수 없습니다."),
+    // PROPOSAL
+    PROPOSAL_NOT_FOUND(HttpStatus.NOT_FOUND, "PROPOSAL400", "제안서를 찾을 수 없습니다."),
     ;
 
     private final HttpStatus httpStatus;
