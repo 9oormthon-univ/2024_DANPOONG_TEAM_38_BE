766784fecfe163439b046789755226f685c5a8ee
88dldl <chltnrgml0999@gmail.com>
2024-11-20 17:05:10 +0900

    refactor: 불필요 코드 삭제 및 password 위치 매핑관계 위로 이동

--- src/main/java/boosters/fundboost/company/domain/Company.java
+++ src/main/java/boosters/fundboost/company/domain/Company.java
@@ -1,6 +1,5 @@
 package boosters.fundboost.company.domain;
 
-import boosters.fundboost.boost.domain.Boost;
 import boosters.fundboost.company.domain.enums.CompanyCategory;
 import boosters.fundboost.global.common.domain.BaseEntity;
 import boosters.fundboost.user.domain.User;
@@ -14,15 +13,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.List;
 
 @Getter
 @Setter
@@ -34,6 +30,7 @@ public class Company extends BaseEntity {
     @Column(name = "company_id")
     private Long id;
     private String businessNumber;
+    private String password;
     @OneToOne(cascade = CascadeType.REMOVE, orphanRemoval = true, fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
