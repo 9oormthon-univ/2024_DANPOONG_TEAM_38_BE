5179e36671d1520c89a477f633d3d5f354593196
88dldl <chltnrgml0999@gmail.com>
2024-11-20 19:27:17 +0900

    feat: 프로젝트 후원 기능 구현

--- src/main/java/boosters/fundboost/boost/service/BoostServiceImpl.java
+++ src/main/java/boosters/fundboost/boost/service/BoostServiceImpl.java
@@ -3,10 +3,12 @@ package boosters.fundboost.boost.service;
 import boosters.fundboost.boost.domain.Boost;
 import boosters.fundboost.boost.repository.BoostRepository;
 import boosters.fundboost.project.domain.Project;
+import boosters.fundboost.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +20,11 @@ public class BoostServiceImpl implements BoostService {
         Page<Boost> boosts = boostRepository.findAllByUser_Id(userId, pageable);
 
         return boosts.map(Boost::getProject);
+    }
+
+    @Override
+    @Transactional
+    public void boostCompany(User user, Project project, long amount) {
+        boostRepository.save(Boost.of(user, project, amount));
     }
 }
