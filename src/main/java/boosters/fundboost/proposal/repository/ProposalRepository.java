3d8954f1c9e91c4a2e87adc639c19aa5dd07a621
88dldl <chltnrgml0999@gmail.com>
2024-11-21 05:26:10 +0900

    feat: company정보로 proposal 검색하는 기능 구현

--- src/main/java/boosters/fundboost/proposal/repository/ProposalRepository.java
+++ src/main/java/boosters/fundboost/proposal/repository/ProposalRepository.java
@@ -1,7 +1,11 @@
 package boosters.fundboost.proposal.repository;
 
+import boosters.fundboost.company.domain.Company;
 import boosters.fundboost.proposal.domain.Proposal;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProposalRepository extends JpaRepository<Proposal, Long> {
+    Page<Proposal> findAllByCompany(Company company, Pageable pageable);
 }
