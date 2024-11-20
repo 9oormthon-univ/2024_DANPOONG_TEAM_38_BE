b1915dbbdfee86544dc255b71702bad1ea464a48
westjuuu <westjiuuu@naver.com>
2024-11-20 16:44:25 +0900

    fix: 일반 사용자도 있으니 company_id null값 허용

--- src/main/java/boosters/fundboost/boost/domain/Boost.java
+++ src/main/java/boosters/fundboost/boost/domain/Boost.java
@@ -29,7 +29,7 @@ public class Boost extends BaseEntity {
     @JoinColumn(name = "project_id", nullable = false)
     private Project project;
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "company_id", nullable = false)
+    @JoinColumn(name = "company_id", nullable = true)
     private Company company;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
