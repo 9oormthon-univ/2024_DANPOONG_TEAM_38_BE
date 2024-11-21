b5bf561abfad2c47a40ce9c4f4452ef2538da46c
88dldl <chltnrgml0999@gmail.com>
2024-11-21 19:22:31 +0900

    feat: 상대의 마이페이지 조회 응답 DTO, 컨버터 작성

--- src/main/java/boosters/fundboost/user/converter/MyPageConverter.java
+++ src/main/java/boosters/fundboost/user/converter/MyPageConverter.java
@@ -1,6 +1,7 @@
 package boosters.fundboost.user.converter;
 
 import boosters.fundboost.user.domain.User;
+import boosters.fundboost.user.dto.response.PeerMyPageResponse;
 import boosters.fundboost.user.dto.response.UserMyPageResponse;
 
 public class MyPageConverter {
@@ -12,6 +13,14 @@ public class MyPageConverter {
                 .tag(user.getTag().getValue())
                 .followingCount(followingCount)
                 .followerCount(followerCount)
+                .build();
+    }
+
+    public static PeerMyPageResponse toPeerMyPageResponse(User user, long followingCount, long followerCount) {
+        return PeerMyPageResponse.builder()
+                .userInfo(toUserMyPageResponse(user, followingCount, followerCount))
+                .title(user.getTitle())
+                .content(user.getContent())
                 .build();
     }
 }
