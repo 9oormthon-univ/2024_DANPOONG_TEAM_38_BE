a2591a3a0b0196bdd7c0f3c1e58fb54aa5200840
88dldl <chltnrgml0999@gmail.com>
2024-11-21 20:21:46 +0900

    feat: 사용자 인증 정보를 추출하기 위한 @AuthInfo 추가

--- src/main/java/boosters/fundboost/global/controller/UserCompanyController.java
+++ src/main/java/boosters/fundboost/global/controller/UserCompanyController.java
@@ -2,6 +2,7 @@ package boosters.fundboost.global.controller;
 
 import boosters.fundboost.global.response.BaseResponse;
 import boosters.fundboost.global.response.code.status.SuccessStatus;
+import boosters.fundboost.global.security.handler.annotation.AuthInfo;
 import boosters.fundboost.global.security.handler.annotation.AuthUser;
 import boosters.fundboost.user.domain.User;
 import boosters.fundboost.user.dto.request.ProfileEditRequest;
@@ -17,7 +18,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,12 +44,12 @@ public class UserCompanyController {
     @Operation(summary = "상대의 마이페이지 조회 API", description = "다른 사람의 마이페이지를 조회합니다._숙희")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
-            @ApiResponse(responseCode = "USER400",description = "USER_NOT_FOUND, 유저를 찾을 수 없습니다."),
-            @ApiResponse(responseCode = "USER402",description = "SELF_PROFILE_ACCESS_NOT_ALLOWED. 해당 API로 본인의 마이페이지는 조회할 수 없습니다."),
+            @ApiResponse(responseCode = "USER400", description = "USER_NOT_FOUND, 유저를 찾을 수 없습니다."),
+            @ApiResponse(responseCode = "USER402", description = "SELF_PROFILE_ACCESS_NOT_ALLOWED. 해당 API로 본인의 마이페이지는 조회할 수 없습니다."),
     })
-    @GetMapping(value = "/{peerId}")
+    @GetMapping(value = "/{peerId}/profile")
     public BaseResponse<PeerMyPageResponse> getPeerProfile(@PathVariable Long peerId,
-                                                           @RequestBody Long userId) {
-        return BaseResponse.onSuccess(SuccessStatus._OK, userService.getPeerProfile(peerId,userId));
+                                                           @Parameter(name = "user", hidden = true) @AuthInfo Long userId) {
+        return BaseResponse.onSuccess(SuccessStatus._OK, userService.getPeerProfile(peerId, userId));
     }
 }
