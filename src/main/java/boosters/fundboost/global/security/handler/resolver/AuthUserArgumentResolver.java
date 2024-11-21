c788699fe0055247e3bfa95b46e8137d93b9fc5d
88dldl <chltnrgml0999@gmail.com>
2024-11-21 20:16:15 +0900

    refactor: 인증정보에서 유저의 이메일을 추출해 리턴하는 로직 분리

--- src/main/java/boosters/fundboost/global/security/handler/resolver/AuthUserArgumentResolver.java
+++ src/main/java/boosters/fundboost/global/security/handler/resolver/AuthUserArgumentResolver.java
@@ -2,13 +2,12 @@ package boosters.fundboost.global.security.handler.resolver;
 
 import boosters.fundboost.global.response.code.status.ErrorStatus;
 import boosters.fundboost.global.security.handler.annotation.AuthUser;
+import boosters.fundboost.global.security.util.SecurityUtils;
 import boosters.fundboost.user.auth.exception.AuthException;
 import boosters.fundboost.user.domain.User;
-import boosters.fundboost.user.exception.UserException;
 import boosters.fundboost.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -35,22 +34,13 @@ public class AuthUserArgumentResolver implements HandlerMethodArgumentResolver {
             NativeWebRequest webRequest,
             WebDataBinderFactory binderFactory) throws AuthException {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Object principal = null;
-
         if (authentication != null) {
             if (authentication.getName().equals("anonymousUser")) {
                 throw new AuthException(ErrorStatus.UNAUTHORIZED_USER);
             }
-            principal = authentication.getPrincipal();
         }
-        if (principal == null || principal.getClass() == String.class) {
-            throw new UserException(ErrorStatus.USER_NOT_FOUND);
-        }
-        UsernamePasswordAuthenticationToken authenticationToken =
-                (UsernamePasswordAuthenticationToken) authentication;
 
-        String userEmail = authenticationToken.getName();
-
+        String userEmail = SecurityUtils.getAuthUserEmail(authentication);
         return userService.getUser(userEmail);
     }
 }
--- src/main/java/boosters/fundboost/global/security/util/SecurityUtils.java
+++ src/main/java/boosters/fundboost/global/security/util/SecurityUtils.java
@@ -1,6 +1,9 @@
 package boosters.fundboost.global.security.util;
 
+import boosters.fundboost.global.response.code.status.ErrorStatus;
 import boosters.fundboost.global.security.userdetails.CustomUserDetails;
+import boosters.fundboost.user.exception.UserException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -12,5 +15,16 @@ public class SecurityUtils {
         }
         CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
         return userDetails.getUserId(); // User ID 반환
+    }
+
+    public static String getAuthUserEmail(Authentication authentication) {
+        Object principal = authentication.getPrincipal();
+        if (principal == null || principal.getClass() == String.class) {
+            throw new UserException(ErrorStatus.USER_NOT_FOUND);
+        }
+        UsernamePasswordAuthenticationToken authenticationToken =
+                (UsernamePasswordAuthenticationToken) authentication;
+
+        return authenticationToken.getName();
     }
 }
