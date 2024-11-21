4611747698c6abf5ce112f9519bfeff21076b383
88dldl <chltnrgml0999@gmail.com>
2024-11-21 22:45:41 +0900

    fix: 카테고리값이 비어있을경우 예외처리 추가

--- src/main/java/boosters/fundboost/company/converter/CompanyRankingConverter.java
+++ src/main/java/boosters/fundboost/company/converter/CompanyRankingConverter.java
@@ -1,10 +1,13 @@
 package boosters.fundboost.company.converter;
 
 import boosters.fundboost.company.domain.Company;
+import boosters.fundboost.company.domain.enums.CompanyCategory;
 import boosters.fundboost.company.dto.response.CompanyRankingPreviewRecord;
 import boosters.fundboost.company.dto.response.CompanyRankingPreviewResponse;
 import boosters.fundboost.company.dto.response.CompanyRankingRecord;
 import boosters.fundboost.company.dto.response.CompanyRankingResponse;
+
+import java.util.Optional;
 
 
 public class CompanyRankingConverter {
@@ -14,11 +17,14 @@ public class CompanyRankingConverter {
                 .id(company.getId())
                 .name(company.getUser().getName())
                 .image(company.getUser().getImage())
-                .category(company.getCategory().getName())
+                .category(Optional.ofNullable(company.getCategory())
+                        .map(CompanyCategory::getName)
+                        .orElse(null))
                 .contributionAmount(companyRankingRecord.contributionAmount())
                 .contributionCount(companyRankingRecord.contributionCount())
                 .build();
     }
+
     public static CompanyRankingPreviewResponse toCompanyRankingPreviewResponse(Company company, CompanyRankingPreviewRecord record) {
         return CompanyRankingPreviewResponse.builder()
                 .companyName(company.getUser().getName())
