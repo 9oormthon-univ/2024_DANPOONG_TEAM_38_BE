a5f86aa2049ad88018c24f6f7ba40f2d1320320d
88dldl <chltnrgml0999@gmail.com>
2024-11-21 19:23:04 +0900

    feat: 상대의 마이페이지 조회하는 API 작성

--- src/main/java/boosters/fundboost/global/controller/UserCompanyController.java
+++ src/main/java/boosters/fundboost/global/controller/UserCompanyController.java
@@ -5,6 +5,7 @@ import boosters.fundboost.global.response.code.status.SuccessStatus;
 import boosters.fundboost.global.security.handler.annotation.AuthUser;
 import boosters.fundboost.user.domain.User;
 import boosters.fundboost.user.dto.request.ProfileEditRequest;
+import boosters.fundboost.user.dto.response.PeerMyPageResponse;
 import boosters.fundboost.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -12,8 +13,11 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,5 +39,17 @@ public class UserCompanyController {
             @ModelAttribute ProfileEditRequest request) {
         userService.editProfile(user, request);
         return BaseResponse.onSuccess(SuccessStatus._NO_CONTENT, null);
+    }
+
+    @Operation(summary = "상대의 마이페이지 조회 API", description = "다른 사람의 마이페이지를 조회합니다._숙희")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
+            @ApiResponse(responseCode = "USER400",description = "USER_NOT_FOUND, 유저를 찾을 수 없습니다."),
+            @ApiResponse(responseCode = "USER402",description = "SELF_PROFILE_ACCESS_NOT_ALLOWED. 해당 API로 본인의 마이페이지는 조회할 수 없습니다."),
+    })
+    @GetMapping(value = "/{peerId}")
+    public BaseResponse<PeerMyPageResponse> getPeerProfile(@PathVariable Long peerId,
+                                                           @RequestBody Long userId) {
+        return BaseResponse.onSuccess(SuccessStatus._OK, userService.getPeerProfile(peerId,userId));
     }
 }
