e546ceab4028182188bf82fc04b6c6f52d1496db
88dldl <chltnrgml0999@gmail.com>
2024-11-21 21:40:15 +0900

    remove: 불필요 칼럼 삭제

--- src/main/java/boosters/fundboost/boost/domain/Boost.java
+++ src/main/java/boosters/fundboost/boost/domain/Boost.java
@@ -1,6 +1,5 @@
 package boosters.fundboost.boost.domain;
 
-import boosters.fundboost.company.domain.Company;
 import boosters.fundboost.global.common.domain.BaseEntity;
 import boosters.fundboost.project.domain.Project;
 import boosters.fundboost.user.domain.User;
@@ -28,9 +27,6 @@ public class Boost extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "project_id", nullable = false)
     private Project project;
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "company_id", nullable = true)
-    private Company company;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
