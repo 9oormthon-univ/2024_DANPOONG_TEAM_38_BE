ef718cdfc496725784e0c4e6c71d4b2d75cc629a
88dldl <chltnrgml0999@gmail.com>
2024-11-23 04:19:58 +0900

    refactor: 후원한 프로젝트 응답에 후원금액 정보 포함되도록 수정

--- src/main/java/boosters/fundboost/boost/repository/BoostRepository.java
+++ src/main/java/boosters/fundboost/boost/repository/BoostRepository.java
@@ -1,19 +1,28 @@
 package boosters.fundboost.boost.repository;
 
 import boosters.fundboost.boost.domain.Boost;
+import boosters.fundboost.project.dto.response.BoostedAmountProjection;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BoostRepository extends JpaRepository<Boost, Long> {
     Page<Boost> findAllByUser_Id(Long userId, Pageable pageable);
+
     Optional<Boost> findByUserIdAndProjectId(Long userId, Long projectId);
 
     long countByProject_Id(Long projectId);
 
     @Query("SELECT COALESCE(SUM(b.amount), 0) FROM Boost b WHERE b.project.id = :projectId")
     long sumAmountByProject_Id(Long projectId);
+
+    @Query("SELECT b.project.id AS projectId, SUM(b.amount) AS boostedAmount " +
+            "FROM Boost b WHERE b.project.id IN :projectIds " +
+            "GROUP BY b.project.id")
+    List<BoostedAmountProjection> findBoostedAmountsByProjectIds(@Param("projectIds") List<Long> projectIds);
 }
